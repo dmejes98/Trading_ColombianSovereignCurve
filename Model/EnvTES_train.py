ab6dfa180ec26502015c94153f4e06518c48838d
DavidMejia <dmejiaestrada98@outlook.es>
2023-03-15 00:22:54 -0500

    Primera versión ambiente de entrenamiento

--- Model/EnvTES_train.py
+++ Model/EnvTES_train.py
@@ -71,4 +71,153 @@ class TESEnvEntr(gym.Env):
         self.trades = 0
         #self.reset()
         self._seed()
+        
+    def _buy_ticker(self, index, action):
+        
+        # perform buy action based on the sign of the action
+        disponible = self.state[0] // self.state[index+1]
+        # print('available_amount:{}'.format(available_amount))
+
+        #update balance
+        self.state[0] -= self.state[index + 1] * min(disponible, action)* \
+                          (1+ CARGO_TRANSACCION)
+
+        self.state[index + TES_DIM + 1] += min(disponible, action)
+
+        self.cost += self.state[index + 1] * min(disponible, action)* \
+                          CARGO_TRANSACCION
+        self.trades += 1
+     
+        
+    def _sell_ticker(self, index, action):
+        
+        # perform sell action based on the sign of the action
+        if self.state[index + TES_DIM + 1] > 0:
+            #update balance
+            self.state[0] += \
+            self.state[index+1] * min(abs(action),self.state[index + TES_DIM + 1]) * \
+             (1- CARGO_TRANSACCION)
+
+            self.state[index + TES_DIM + 1] -= min(abs(action), self.state[index + TES_DIM + 1])
+            self.cost +=self.state[index+1]*min(abs(action),self.state[index + TES_DIM + 1]) * \
+             CARGO_TRANSACCION
+            self.trades+=1
+        else:
+            pass
+
+
+    def step(self, actions):
+        # print(self.day)
+        self.terminal = self.day >= len(self.df.index.unique())-1
+        # print(actions)
+
+        if self.terminal:
+            plt.plot(self.asset_memory,'r')
+            plt.savefig('/images/train/account_value_train.png')
+            plt.close()
+            end_total_asset = self.state[0]+ \
+            sum(np.array(self.state[1:(TES_DIM+1)])*np.array(self.state[(TES_DIM+1):(TES_DIM*2+1)]))
+            
+            #print("end_total_asset:{}".format(end_total_asset))
+            df_total_value = pd.DataFrame(self.asset_memory)
+            df_total_value.to_csv('/csv/train/account_value_train.csv')
+            #print("total_reward:{}".format(self.state[0]+sum(np.array(self.state[1:(STOCK_DIM+1)])*np.array(self.state[(STOCK_DIM+1):61]))- INITIAL_ACCOUNT_BALANCE ))
+            #print("total_cost: ", self.cost)
+            #print("total_trades: ", self.trades)
+            df_total_value.columns = ['account_value']
+            df_total_value['daily_return']=df_total_value.pct_change(1)
+            sharpe = (252**0.5)*df_total_value['daily_return'].mean()/ \
+                  df_total_value['daily_return'].std()
+            #print("Sharpe: ",sharpe)
+            #print("=================================")
+            df_rewards = pd.DataFrame(self.rewards_memory)
+            #df_rewards.to_csv('/kaggle/working/account_rewards_train.csv')
+            
+            # print('total asset: {}'.format(self.state[0]+ sum(np.array(self.state[1:29])*np.array(self.state[29:]))))
+            #with open('obs.pkl', 'wb') as f:  
+            #    pickle.dump(self.state, f)
+            
+            return self.state, self.reward, self.terminal,{}
+
+        else:
+            # print(np.array(self.state[1:29]))
+
+            actions = actions * TES_NORMALIZE
+            #actions = (actions.astype(int))
+            
+            begin_total_asset = self.state[0]+ \
+            sum(np.array(self.state[1:(TES_DIM+1)])*np.array(self.state[(TES_DIM+1):(TES_DIM*2+1)]))
+            #print("begin_total_asset:{}".format(begin_total_asset))
+            
+            argsort_actions = np.argsort(actions)
+            
+            sell_index = argsort_actions[:np.where(actions < 0)[0].shape[0]]
+            buy_index = argsort_actions[::-1][:np.where(actions > 0)[0].shape[0]]
+
+            for index in sell_index:
+                # print('take sell action'.format(actions[index]))
+                self._sell_stock(index, actions[index])
+
+            for index in buy_index:
+                # print('take buy action: {}'.format(actions[index]))
+                self._buy_stock(index, actions[index])
+
+            self.day += 1
+            self.data = self.df.loc[self.day,:]         
+            #load next state
+            # print("stock_shares:{}".format(self.state[29:]))
+
+            self.state = [self.state[0]] + \
+                          self.data.Tasa.values.tolist() + \
+                          self.data["Precio Limpio"].values.tolist() + \
+                          self.data["Precio Sucio"].values.tolist() + \
+                          list(self.state[(TES_DIM+1):(TES_DIM*2+1)]) + \
+                          self.data["Duración"].values.tolist() + \
+                          self.data["Duración Modificada"].values.tolist() + \
+                          self.data.Dv01.values.tolist() + \
+                          self.data.Convexidad.values.tolist()
+
+            end_total_asset = self.state[0]+ \
+            sum(np.array(self.state[1:(TES_DIM+1)])*np.array(self.state[(TES_DIM+1):(TES_DIM*2+1)]))
+            self.asset_memory.append(end_total_asset)
+            #print("end_total_asset:{}".format(end_total_asset))
+            
+            self.reward = end_total_asset - begin_total_asset            
+            # print("step_reward:{}".format(self.reward))
+            self.rewards_memory.append(self.reward)
+            
+            self.reward = self.reward*REWARD_SCALING
+
+
+
+        return self.state, self.reward, self.terminal, {}
+
+    def reset(self):
+        self.asset_memory = [BALANCE_INICIAL_CUENTA]
+        self.day = 0
+        self.data = self.df.loc[self.day,:]
+        self.cost = 0
+        self.trades = 0
+        self.terminal = False 
+        self.rewards_memory = []
+        #initiate state
+        self.state = [BALANCE_INICIAL_CUENTA] + \
+                      self.data.Tasa.values.tolist() + \
+                      self.data["Precio Limpio"].values.tolist() + \
+                      self.data["Precio Sucio"].values.tolist() + \
+                      [0]*TES_DIM + \
+                      self.data["Duración"].values.tolist() + \
+                      self.data["Duración Modificada"].values.tolist() + \
+                      self.data.Dv01.values.tolist() + \
+                      self.data.Convexidad.values.tolist()
+        # iteration += 1 
+        return self.state
     
+    def render(self, mode='human'):
+        return self.state
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+
